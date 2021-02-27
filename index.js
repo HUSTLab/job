59ae2e36a4c1d870aab6c105d163016b00a2c37b
mayandev <phillzou@gmail.com>
2021-02-27 23:28:21 +0800

    fix: add subscribe guide

--- index.js
+++ index.js
@@ -60,6 +60,9 @@ async function updateReadme() {
 
 打开一个 [Issue](https://github.com/HUSTLab/job/issues/new?assignees=&labels=&template=------.md&title=%E6%A0%87%E9%A2%98%E6%A8%A1%E7%89%88%EF%BC%9A%E9%98%BF%E9%87%8C%E5%B7%B4%E5%B7%B4-%E5%89%8D%E5%90%8E%E7%AB%AF-%E6%9D%AD%E5%B7%9E-%E5%85%B6%E4%BB%96%E4%BF%A1%E6%81%AF)，按照 Issues 模版填写相关的招聘信息，并选择一个标签，提交后会自动更新 Readme。
 
+## 如何订阅
+点击右上方 Watch 徽标，选择 Custom 并设置监听 issues，信息更新时您将收到邮件提醒。
+
   `;
   if (!isEmpty(topIssues)) {
     // 添加置顶信息
@@ -93,6 +96,7 @@ async function updateReadme() {
     });
     readme += partMD;
   }
+
   fs.writeFileSync('./README.md', readme, 'utf8');
 }
 
